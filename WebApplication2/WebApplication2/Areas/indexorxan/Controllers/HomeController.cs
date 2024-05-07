e2c24db3295acc18313e30dd524448859a378444
orxanmvv <m.orxan.m123@gmail.com>
2024-05-07 11:27:53 +0400

    mvc task

---
+++ WebApplication2/WebApplication2/Areas/indexorxan/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Areas.indexorxan.Controllers
+{
+
+    [Area("indexorxan")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
